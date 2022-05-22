b2a72d43019eeb1f26649b97fef3796a4c9979e6
chenxizhang <chenxizhang@devbox>
2022-05-22 10:37:16 +0000

    添加项目文件。

---
+++ HelloAction.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Flow.RPA.Desktop.Modules.SDK;
+using Microsoft.Flow.RPA.Desktop.Modules.SDK.Attributes;
+using Microsoft.Flow.RPA.Desktop.Modules.SDK.Extended.Attributes;
+using System;
+
+namespace Xizhang.Modules.Helloworld.Actions
+{
+    [Action(Id = "Hello")]
+    [Icon(Code = "EEE7")]
+    public class HelloAction : ActionBase
+    {
+        [InputArgument]
+        public string Name { get; set; }
+
+        [OutputArgument]
+        public string Result { get; set; }
+        public override void Execute(ActionContext context)
+        {
+
+            Result = String.Format(Properties.Resources.Messages_SayHello, Name);
+        }
+    }
+}
